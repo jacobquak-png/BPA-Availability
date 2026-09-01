1362b916681899d2b2ea7692a4957719069e7207
jacobquak-png <jacob.quak@gmail.com>
2026-09-01 13:54:22 +0200

    Update bpa_beheer_ui.py

--- bpa_beheer_ui.py
+++ bpa_beheer_ui.py
@@ -358,11 +358,10 @@ if "overzicht_df" not in st.session_state:
 #  TABS
 # ══════════════════════════════════════════════════════════════════════════════
 
-tab_overzicht, tab_classificatie, tab_subscripties, tab_toevoegen, tab_verwijderen, tab_klanten= st.tabs([
+tab_overzicht, tab_classificatie, tab_subscripties, tab_verwijderen, tab_klanten= st.tabs([
     "Overzicht 99%",
     "Componenten selecteren",
     "Gegevens aanpassen",
-    "Component toevoegen",
     "Component verwijderen",
     "Klanten & contracten",
 ])
